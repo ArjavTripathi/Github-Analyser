712d1ac85a0ecf95e7f5a05616c4cecccbd2a4e0
ArjavTripathi <arjavtri@iastate.edu>
2026-06-14 18:02:49 -0500

    added supabase support

--- backend/schemas/models.py
+++ backend/schemas/models.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine, Column, String, Boolean, DateTime, JSON, Integer, inspect, text
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from datetime import datetime
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "sqlite:///./app.db"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
